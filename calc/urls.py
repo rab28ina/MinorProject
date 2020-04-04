87de08143ac4c1b7cae305e65236f31f59d0a576
nishasen <43262505+nishasen@users.noreply.github.com>
2020-04-04 14:00:37 +0530

    Update urls.py

--- calc/urls.py
+++ calc/urls.py
@@ -4,9 +4,9 @@ from . import views
 urlpatterns = [
     path('',views.index,name='index'),
     path('about',views.about,name='about'),
-    path('AddCourse',views.AddCourse,name='AddCourse'),
-    path('AddTeacher',views.AddTeacher,name='AddTeacher'),
+    path('AddCourse',views.addcourse,name='AddCourse'),
+    path('AddTeacher',views.Teachers,name='AddTeacher'),
     path('classroom',views.classroom,name='classroom'),
-    path('CreateTimeTable',views.CreateTimeTable,name='CreateTimeTable')
+    path('CreateTimeTable',views.createtimetable,name='CreateTimeTable')
    
 ]
